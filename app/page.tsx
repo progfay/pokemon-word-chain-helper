8d1ff9bdf9fb979d269582877ba62499c8a56bc6
progfay <19568747+progfay@users.noreply.github.com>
2025-06-21 14:00:29 +0900

    add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from "next";
 import { PokemonApp } from "./components/PokemonApp";
 import { loadPokemonDatabase } from "./lib/pokemon-database-loader";
+
+export const metadata: Metadata = {
+	title: "ポケモンしりとりヘルパー",
+	description: 'Web application that assists players in "ポケモンしりとり".',
+	authors: [{ name: "@progfay", url: "https://github.com/progfay" }],
+};
 
 export default async function Home() {
 	const pokemonDatabase = await loadPokemonDatabase();
