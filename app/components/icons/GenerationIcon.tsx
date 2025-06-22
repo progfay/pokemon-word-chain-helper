2abc3de3f05530359421219218b80cf048709d30
progfay <19568747+progfay@users.noreply.github.com>
2025-06-22 18:22:53 +0900

    update icon svg

--- app/components/icons/GenerationIcon.tsx
+++ app/components/icons/GenerationIcon.tsx
@@ -12,8 +12,30 @@ export function GenerationIcon(props: SVGProps<SVGSVGElement>) {
 			{...props}
 		>
 			<title>Generation Icon</title>
-			<path d="M2.25 4.5h13.5v9a1.5 1.5 0 0 1-1.5 1.5H3.75a1.5 1.5 0 0 1-1.5-1.5v-9Z" />
-			<path d="M2.25 2.25h13.5v2.25H2.25v-2.25ZM6 8.25h6M6 11.25h6" />
+			<path
+				d="M6 1.5V4.5"
+				stroke-width="1.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+			<path
+				d="M12 1.5V4.5"
+				stroke-width="1.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+			<path
+				d="M14.25 3H3.75C2.92157 3 2.25 3.67157 2.25 4.5V15C2.25 15.8284 2.92157 16.5 3.75 16.5H14.25C15.0784 16.5 15.75 15.8284 15.75 15V4.5C15.75 3.67157 15.0784 3 14.25 3Z"
+				stroke-width="1.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+			<path
+				d="M2.25 7.5H15.75"
+				stroke-width="1.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
 		</svg>
 	);
 }
