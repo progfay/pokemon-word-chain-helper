a3d54e1a15e118587150801ee4229a72cde7baf4
progfay <19568747+progfay@users.noreply.github.com>
2025-06-22 22:55:19 +0900

    add GitHub Workflow for deploying GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  basePath: "/pokemon-word-chain-helper",
+  output: "export",
+};
 
 export default nextConfig;
