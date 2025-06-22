e56d2f0787ba25b6cf5eed487ed09c1d384791f0
progfay <19568747+progfay@users.noreply.github.com>
2025-06-22 23:08:30 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
+// @ts-check
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   basePath: "/pokemon-word-chain-helper",
   output: "export",
 };
